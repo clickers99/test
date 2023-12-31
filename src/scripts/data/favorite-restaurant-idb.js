7dbdd12a3f206404b8f5ff4c8b749f25127005ea
Muhammad Irham Arfansyah <137649025+clickers99@users.noreply.github.com>
2024-01-01 02:07:19 +0700

    first commit

---
+++ src/scripts/data/favorite-restaurant-idb.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-prototype-builtins */
+import { openDB } from 'idb';
+import CONFIG from '../globals/config';
+
+const { DATABASE_NAME, DATABASE_VERSION, OBJECT_STORE_NAME } = CONFIG;
+
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade(database) {
+    database.createObjectStore(OBJECT_STORE_NAME, { keyPath: 'id' });
+  },
+});
+
+const FavoriteRestaurantIdb = {
+  async getRestaurant(id) {
+    if (!id) {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return (await dbPromise).get(OBJECT_STORE_NAME, id);
+  },
+
+  async getAllRestaurants() {
+    const result = await (await dbPromise).getAll(OBJECT_STORE_NAME);
+    return Array.isArray(result) ? result : [];
+  },
+
+  async putRestaurant(restaurant) {
+    if (!restaurant.hasOwnProperty('id')) {
+      return;
+    }
+    // eslint-disable-next-line consistent-return
+    return (await dbPromise).put(OBJECT_STORE_NAME, restaurant);
+  },
+
+  async deleteRestaurant(id) {
+    return (await dbPromise).delete(OBJECT_STORE_NAME, id);
+  },
+};
+
+export default FavoriteRestaurantIdb;
---
+++ src/scripts/views/pages/home.js
@@ -0,0 +1,33 @@
+import RestaurantDbSource from '../../data/restaurantdb-source';
+import { createRestaurantItemTemplate } from '../template/template';
+
+const Home = {
+  async render() {
+    return `
+    <div class="hero">
+      <div class="hero__inner">
+        <h1 class="hero__title" tabindex="0">Selamat Datang</h1>
+        <h2 class="hero__tagline" tabindex="0">
+        Jelajahi Restoran Pilihan Terbaik di Sekitar Anda dengan Mudah!
+        </h2>
+      </div>
+    </div>
+    <div id="mainContent" class="contents">
+      <h1 id="explore" tabindex="0">Explore Restaurant</h1>
+      <div id="restaurants">
+        
+      </div>
+    </div>
+      `;
+  },
+
+  async afterRender() {
+    const restaurants = await RestaurantDbSource.restaurantList();
+    const restaurantContainer = document.querySelector('#restaurants');
+    restaurants.forEach((restaurant) => {
+      restaurantContainer.innerHTML += createRestaurantItemTemplate(restaurant);
+    });
+  },
+};
+
+export default Home;
